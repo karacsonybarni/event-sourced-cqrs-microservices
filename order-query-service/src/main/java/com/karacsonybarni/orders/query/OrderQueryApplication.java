6e9ee091828855d4ad0154804d424219c4c190e8
Barna Karacsony <karacsony.barni@gmail.com>
2026-08-30 20:56:38 +0200

    Remove projector-only clock from query API

--- order-query-service/src/main/java/com/karacsonybarni/orders/query/OrderQueryApplication.java
+++ order-query-service/src/main/java/com/karacsonybarni/orders/query/OrderQueryApplication.java
@@ -1,12 +1,9 @@
 package com.karacsonybarni.orders.query;
 
-import java.time.Clock;
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @OpenAPIDefinition(info = @Info(
         title = "Order Query API",
@@ -17,10 +14,5 @@ public class OrderQueryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderQueryApplication.class, args);
-    }
-
-    @Bean
-    Clock clock() {
-        return Clock.systemUTC();
     }
 }
